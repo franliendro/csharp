986ecd7f46adf9c904b24f2dc823ee3eab1ed9c8
Fran <73044721+franliendro@users.noreply.github.com>
2022-09-16 19:06:04 -0300

    Add files via upload

---
+++ sesion2/Ej1.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Ingresar nombre");
+string name= Console.ReadLine();
+Console.WriteLine("Ingresar apellido");
+string surname = Console.ReadLine();
+Console.WriteLine("Ingresar edad");
+int years= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Sabe programar? YES or NO");
+string prog = Console.ReadLine();
+string user = "Nombre: " + name + " Apellido: " + surname +" Edad: "+years+" Sabe programar? "+prog; 
---
+++ sesion2/Ej3.cs
@@ -0,0 +1,6 @@
+﻿int num= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(num>=18);
+char c = Convert.ToChar(Console.ReadLine());
+Console.WriteLine(c == 'a');
+Console.WriteLine(num >= 18 && c == 'a');
+Console.WriteLine(num >= 18 || c == 'a');
