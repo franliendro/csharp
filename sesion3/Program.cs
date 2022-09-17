bb26d6f43ee3513531f6050309a517acaa483b04
Fran <73044721+franliendro@users.noreply.github.com>
2022-09-17 14:44:55 -0300

    Add files via upload

---
+++ sesion3/Program.cs
@@ -0,0 +1,20 @@
+﻿Cliente cliente = new Cliente("Fernando",432432,"Argentina","email@gmail.com", true);
+Console.WriteLine(cliente);
+public struct Cliente
+{
+    public Cliente(string nombre, int tel, string dir, string email, bool nuevo)
+    {
+        NOM = nombre;
+        TEL = tel;
+        DIR = dir;
+        EMAIL = email;
+        NUEVO = nuevo;
+    }
+    public string NOM { get; }
+    public int TEL { get; }
+    public string DIR { get; }
+    public string EMAIL { get; }
+    public bool NUEVO { get; }
+
+    public override string ToString() => $"(Nombre: {NOM}, Telefono: {TEL}, Direccion: {DIR}, Email: {EMAIL}, Nuevo: {NUEVO})";
+}
