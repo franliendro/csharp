3ae29364c16263b9fefbc87948c7a864faeaf5c2
Fran <73044721+franliendro@users.noreply.github.com>
2022-09-19 18:10:23 -0300

    Add files via upload

---
+++ sesion5/Ej 1.cs
@@ -0,0 +1,27 @@
+﻿float precio = 45;
+Cliente cliente = new Cliente();
+Console.WriteLine("Escriba nombre: ");
+cliente.NOM = Console.ReadLine();
+Console.WriteLine("Escriba email: "); ;
+cliente.EMAIL = Console.ReadLine();
+Console.WriteLine("Escriba si tiene cupon: ");
+cliente.CUPON = Convert.ToBoolean(Console.ReadLine());
+Console.WriteLine(cliente);
+if (cliente.CUPON == true) Console.WriteLine($"El precio del producto es {precio} pero con el descuento es {precio/2}");
+else Console.WriteLine($"El precio del producto es {precio}");
+public struct Cliente
+{
+    public Cliente(string nom, string email,bool cupon)
+    {
+        
+        NOM = nom;
+        EMAIL = email;
+        CUPON = cupon;
+    }
+
+    public string NOM { get; set; }
+    public string EMAIL { get; set; }
+    public bool CUPON { get; set; }
+
+    public override string ToString() => $"({NOM}, {EMAIL},{CUPON})";
+}
---
+++ sesion5/Ej 2.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Opcion a: C#\nOpcion b: Java\nOpcion c: C++");
+char opc = Convert.ToChar(Console.ReadLine());
+switch (opc)
+{
+    case 'a': Console.WriteLine("Hola, mundo C#");
+        break;
+    case 'b': Console.WriteLine("Java");
+        break ;
+    case 'c': Console.WriteLine("C++");
+        break;
+    default: Console.WriteLine("Ninguno fue seleccionado");
+        break;
+}
